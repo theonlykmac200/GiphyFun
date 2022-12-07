ab782f4ee2c40d7d49c094f327ce570343ebc74a
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-12-07 00:11:18 -0800

    style

--- src/components/GiphyDisplay.js
+++ src/components/GiphyDisplay.js
@@ -3,15 +3,16 @@ import {useState} from "react"
 const GiphyDisplay = ({giphy}) => {
     const num = Math.floor(Math.random() * 50)
     return (
-            // giphy.embed_url ?
+           
         <>
-        {/* <img src={giphy.data.slug}/> */}
-        <h1>{giphy?.data[num].title}</h1>
+        <h1> click the button to get your trending Giphy</h1>
+       
         <iframe src={giphy?.data[num].embed_url}></iframe>
+        <h1>{giphy?.data[num].title}</h1>
 
         </>
-        // :
-        // <h1> click the button to get your trending Giphy</h1>
+    
+        
     )
 }
 
