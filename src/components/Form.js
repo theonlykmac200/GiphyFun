df8dac4a733eb4fed9e5ca3b5496a968be2c16f2
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-12-06 21:34:32 -0800

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import {useState} from "react"
+
+const Form = (props) => {
+    return (
+        <>
+        <h1>This is the Form Component</h1>
+        </>
+    )
+}
+
+export default Form
---
+++ src/components/GiphyDisplay.js
@@ -0,0 +1,11 @@
+import {useState} from "react"
+
+const GiphyDisplay = (props) => {
+    return (
+        <>
+        <h1> The Giphy will appear here</h1>
+        </>
+    )
+}
+
+export default GiphyDisplay
