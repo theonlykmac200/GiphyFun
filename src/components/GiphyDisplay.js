ba63d0270c9148518477bb5dd7787a35db599fc8
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-12-06 23:35:28 -0800

    render iframe embeded url

--- src/components/GiphyDisplay.js
+++ src/components/GiphyDisplay.js
@@ -4,8 +4,9 @@ const GiphyDisplay = ({giphy}) => {
     return (
             // giphy.embed_url ?
         <>
-        {/* <img src={giphy.data.embed_url}/> */}
-        <h1>{giphy?.data.source}</h1>
+        {/* <img src={giphy.data.slug}/> */}
+        <h1>{giphy?.data.title}</h1>
+        <iframe src={giphy?.data.embed_url}></iframe>
         </>
         // :
         // <h1> click the button to get your trending Giphy</h1>
