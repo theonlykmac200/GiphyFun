aa74f289774af5a2f823376c8efafb1297a3e8eb
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-12-17 18:41:12 -0700

    add feedback

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 live link https://friendly-nasturtium-df291f.netlify.app
 
+I would make this a file and display as an image so whomever comes to your readme doesn't have to download it to their computer.
+# The component diagram looks great, it will be very useful for any React app you make.
+
 Structure of the App.
 [App.pdf](https://github.com/theonlykmac200/GiphyFun/files/10224359/App.pdf)
--- src/components/GiphyDisplay.js
+++ src/components/GiphyDisplay.js
@@ -1,13 +1,16 @@
 import {useState} from "react"
 
-const GiphyDisplay = ({giphy}) => {
+const GiphyDisplay = ({ giphy }) => {
+    //I love this!! any extra JS practice and displaying the data randomly for trending is great!
+
+    //you are getting a warning for iframe, needs title property, check for these warnings and fix them see below
     const num = Math.floor(Math.random() * 50)
     return (
            
         <>
         <h1> click the button to get your trending Giphy</h1>
        
-        <iframe src={giphy?.data[num].embed_url}></iframe>
+        <iframe title='trending gif'src={giphy?.data[num].embed_url}></iframe>
         <h1>{giphy?.data[num].title}</h1>
 
         </>
